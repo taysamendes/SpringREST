e33ef4670e2f28469bfd773346f2bf5571358b0d
TaysaMendes <taysa_smp@hotmail.com>
2020-06-20 16:15:31 -0300

    projeto base

---
+++ service-order-api/src/test/java/com/algaworks/osapi/ServiceOrderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.osapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceOrderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
