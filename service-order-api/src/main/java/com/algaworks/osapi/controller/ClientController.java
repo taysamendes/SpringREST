9c6eb76e3db2183fe7370deeac1452e9067b5fe6
TaysaMendes <taysa_smp@hotmail.com>
2020-06-20 16:33:52 -0300

    classe cliente

--- service-order-api/src/main/java/com/algaworks/osapi/controller/ClientController.java
+++ service-order-api/src/main/java/com/algaworks/osapi/controller/ClientController.java
@@ -1,13 +1,28 @@
 package com.algaworks.osapi.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.algaworks.osapi.domain.model.Client;
 
 @RestController
 public class ClientController {
 	
-	@GetMapping("/clientes")
-	public String listar() {
-		return "clientes";
+	@GetMapping("/clients")
+	public List<Client> listar() {
+		var client1 = new Client();
+		client1.setIdLong(1L);
+		client1.setName("Andreza");
+		client1.setEmail("ad@hotmail.com");
+		
+		var client2 = new Client();
+		client2.setIdLong(2L);
+		client2.setName("Douglas");
+		client2.setEmail("douglas@hotmail.com");
+		
+		return Arrays.asList(client1, client2);
 	}
 }
---
+++ service-order-api/src/main/java/com/algaworks/osapi/domain/model/Client.java
@@ -0,0 +1,44 @@
+package com.algaworks.osapi.domain.model;
+
+public class Client {
+	
+	private Long idLong;
+	private String name;
+	private String email;
+	private String phone;
+	
+	public Long getIdLong() {
+		return idLong;
+	}
+	
+	public void setIdLong(Long idLong) {
+		this.idLong = idLong;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getPhone() {
+		return phone;
+	}
+	
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	
+
+}
